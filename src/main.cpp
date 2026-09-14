94fdcb126fc7fd59041660088a3c73e271cce2d8
원준 <apppppp8229@gmail.com>
2026-09-14 20:20:39 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <sstream>
 
 int main() {
-  // Flush after every std::cout / std:cerr
+  // 출력이 버퍼에 남지 않도록 즉시 flush 한다.
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
@@ -12,15 +12,30 @@ int main() {
 
   std::string line;
 
+  // 쉘은 명령어를 계속 받기 때문에 while문을 돌렸다.
   while (true)
   {
-    std::cout << "$ ";  
+    std::cout << "$ "; 
+    // 그냥 cin을 쓰면 공백을 만나면 멈추기 때문에 getline을 썼다.
     std::getline(std::cin, line);
+    // 공백을 기준으로 세기 때문에 stringstream을 썼다.
     std::stringstream ss(line);
     std::string word;
     ss >> word;
-
-    if (word == "exit")
+    
+    if (word == type)
+    {
+      ss >> word;
+      if (word == "echo" || word == "exit" || word == "type")
+      {
+        std::cout << word << " is a shell builtin";
+      }
+      else 
+      {
+        std::cout << word << ": not found";
+      }
+    }
+    else if (word == "exit")
     {
       return 0;
     }
@@ -29,7 +44,7 @@ int main() {
       while (ss >> word)
       {
         std::cout << word << " ";
-    }
+      }
     }
     else {
       std::cout << word;
