1c08531347ee48bb7c5e018b4abb1e873bab3cea
kingwonjun <apppppp8229@gmail.com>
2026-09-16 05:01:36 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@ int main() {
             // 인자의 개수와 인자값이 정상적으로 출력될려면
             // stringstream을 초기화하고 처음 위치로 되돌려야한다.
             ss.clear();
-            ss.seekg(1);
+            ss.seekg(0);
             bool fileFound = false;
             while (getline(pp, path_env, ':')) {
                 for (const auto &entry: fs::directory_iterator(path_env)) {
