72791df6ec1705cf11754ca5bca1cb3b79a989cb
kingwonjun <apppppp8229@gmail.com>
2026-09-15 18:16:29 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -87,7 +87,7 @@ int main() {
                         for (int i = 0; i < argv_vector.size(); i++) {
                             argv[i] = argv_vector[i];
                         }
-                        argv[argv_vector.size()] = nullptr;
+                        argv[argv_vector.size() + 1] = nullptr;
                         //파일 실행과 인자를 넣음
                         // c++ 17 이상에서는 string타입의 word가 data()를 붙이면 char* 된다. c_str()은 const char*이 되고, data()는 수정이 된다.
                         // data()가 조금 더 현대적이라고 한다.
