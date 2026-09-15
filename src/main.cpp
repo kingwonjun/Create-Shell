8050e50063a48e294318acce19fc98a2cfa8fec2
kingwonjun <apppppp8229@gmail.com>
2026-09-16 05:05:00 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -108,8 +108,8 @@ int main() {
                 }
                 if (fileFound == false){
                     std::cout << word << ": not found";
-                    break;
                 }
+                break;
             }
         }
         std::cout << "\n";
