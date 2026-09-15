f7d8824978066f0fc8209c705e32186a5784efd3
kingwonjun <apppppp8229@gmail.com>
2026-09-15 13:52:24 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,10 @@ int main() {
 
         if (word == "type") {
             // word가 "type"이 들어가고 type 다음에 바로 공백이 나와서 erase로 지움
-            word.erase(0, 1);
             while (getline(ss, word, ':')) {
+                if (word[0] == ' ') {
+                    word.erase(0, 1);
+                }
                 if (word == "echo" || word == "exit" || word == "type") {
                     std::cout << word << " is a shell builtin";
                     break;
