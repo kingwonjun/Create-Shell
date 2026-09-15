c5e807d47fb8eec8ec6d0004cdd27adb2266d7c0
kingwonjun <apppppp8229@gmail.com>
2026-09-16 04:55:54 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -107,7 +107,6 @@ int main() {
                         break;
                     }
                 }
-                printf("애초에 여기까지 가나?\n");
                 if (fileFound == false){
                     std::cout << word << ": not found";
                     break;
