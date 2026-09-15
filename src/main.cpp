4348dc6fe4e861486b85b2245ad0baf1c002ab50
kingwonjun <apppppp8229@gmail.com>
2026-09-15 13:39:56 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -32,14 +32,22 @@ int main() {
                     std::cout << word << " is a shell builtin";
                     break;
                 }
-                printf("Word : %s\n", word.c_str());
-                for (const auto &entry: fs::directory_iterator(word)) {
+                // printf("Word : %s\n", word.c_str());
+                for (const auto &entry: fs::directory_iterator("/usr/bin")) {
                     // 파일이름이 word와 일치하고, 그냥 파일일 때 `is regular_file()`, 실행권한이 있는 파일일 때
                     if (entry.path().filename() == word && entry.is_regular_file() == true && access(
                             entry.path().c_str(), X_OK) == 0) {
                         std::cout << word << " is " << entry.path();
                         break;
                     }
+                }
+                for (const auto &entry: fs::directory_iterator("/usr/local/bin")) {
+                    // 파일이름이 word와 일치하고, 그냥 파일일 때 `is regular_file()`, 실행권한이 있는 파일일 때
+                    if (entry.path().filename() == word && entry.is_regular_file() == true && access(
+                            entry.path().c_str(), X_OK) == 0) {
+                        std::cout << word << " is " << entry.path();
+                        break;
+                            }
                 }
                 std::cout << word << ": not found";
             }
