35b127565ff70bd5b04c8a3e6dace6773896de09
kingwonjun <apppppp8229@gmail.com>
2026-09-15 18:00:53 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -80,13 +80,13 @@ int main() {
                         std::string command;
                         std::vector<char *> argv_vector;
                         while (ss >> word) {
-                            printf("word: %s\n", word.c_str());
                             argv_vector.push_back(word.data());
                         }
                         // execvp는 마지막에 종료를 알리는 nullptr이 필요해서 size() + 1을 하였다.
                         char *argv[argv_vector.size() + 1];
                         for (int i = 0; i < argv_vector.size(); i++) {
                             argv[i] = argv_vector[i];
+                            printf("argv[%d] = %s\n", i, argv[i]);
                         }
                         argv[argv_vector.size()] = nullptr;
                         //파일 실행과 인자를 넣음
