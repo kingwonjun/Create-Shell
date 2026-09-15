d95b71de12351c295904af66371aed1e9a531415
kingwonjun <apppppp8229@gmail.com>
2026-09-15 13:37:09 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main() {
                     std::cout << word << " is a shell builtin";
                     break;
                 }
-                // printf("Word : %s\n", word.c_str());
+                printf("Word : %s\n", word.c_str());
                 for (const auto &entry: fs::directory_iterator(word)) {
                     // 파일이름이 word와 일치하고, 그냥 파일일 때 `is regular_file()`, 실행권한이 있는 파일일 때
                     if (entry.path().filename() == word && entry.is_regular_file() == true && access(
